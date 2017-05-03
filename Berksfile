dbd0970ced0c1ab0d6c7a673a329d71574055809
randomcoww <etherblimp@gmail.com>
2017-05-02 22:08:46 -0700

    add kea zonefile

--- Berksfile
+++ Berksfile
@@ -3,4 +3,5 @@ source "https://supermarket.chef.io"
 metadata
 
 cookbook "nsd_resource", github: "randomcoww/chef-nsd_resource"
+cookbook "mysql-cluster", github: "randomcoww/chef-mysql-cluster"
 cookbook "dbag", github: "randomcoww/chef-dbag"
---
+++ libraries/resource/kea_zonefile.rb
@@ -0,0 +1,60 @@
+require 'ipaddr'
+
+class ChefNsd
+  class Resource
+    class KeaZonefile < Chef::Resource
+      include NsdConfigGenerator
+      include MysqlConfig
+
+      resource_name :nsd_kea_zonefile
+
+      default_action :create
+      allowed_actions :create
+
+      property :domain, String
+      property :ttl, Integer, default: 300
+      property :name_server, String
+      property :email_addr, String
+      property :sn, Integer, default: 2017010101
+      property :ref, Integer, default: 28800
+      property :ret, Integer, default: 14400
+      property :ex, Integer, default: 604800
+      property :nx, Integer, default: 86400
+
+      property :host, String
+      property :database, String
+      property :username, String
+      property :password, String
+      property :timeout, Integer, default: 120
+      property :hosts, Hash, default: lazy { get_lease_hosts }
+
+      property :zone_options, Hash, default: {}
+      property :path, String, default: lazy { ::File.join(Chef::Config[:file_cache_path], 'nsd', domain) }
+    end
+
+    def provider
+      ChefNsd::Provider::Zonefile
+    end
+
+
+    private
+
+    def get_lease_hosts
+      result = {}
+
+      client = MysqlConfig::Client.new(timeout,
+        username: username,
+        database: database,
+        host: host,
+        password: password
+      )
+
+      query = %Q{SELECT hostname,address FROM lease4 WHERE client_id IS NOT NULL AND hostname!="" AND state=0'}
+      client.query(query).each do |e|
+        result[e['hostname']] = e['address']
+      end
+
+      return result
+    end
+  end
+end
